81215513e04f69c1358af08e490192354ca6c605
Violetta Bocharova <chernovavioletta95@gmail.com>
2021-08-22 16:07:36 +0300

    Initial commit

---
+++ 2.2-databases-2/m2m-relations/website/settings_local.py
@@ -0,0 +1,14 @@
+import os
+from .settings import BASE_DIR
+
+SECRET_KEY = 'd+mw&mscg5i&tx+#@bf+6m%e+d5z!u#!n%z-^o9u7y1felv2o&'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'netology_homework_db',
+        'USER': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
